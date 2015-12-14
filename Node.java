6c5ff66e67d46542c1eee1d9bb767d88e16989b7
pianonerd7 <pianonerd7@gmail.com>
2015-12-14 15:28:15 -0500

    added node class

---
+++ Node.java
@@ -0,0 +1,19 @@
+
+public class Node {
+
+	private int firstIndex;
+	private int secondIndex;
+	
+	public Node (int firstIndex, int secondIndex) {
+		this.firstIndex = firstIndex;
+		this.secondIndex = secondIndex;
+	}
+	
+	public int getFirstIndex() {
+		return firstIndex;
+	}
+	
+	public int getSecondIndex() {
+		return secondIndex;
+	}
+}
