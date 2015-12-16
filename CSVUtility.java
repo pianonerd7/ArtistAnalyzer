93ada782b150704bf7b8ee13ef89b77d144cee81
pianonerd7 <pianonerd7@gmail.com>
2015-12-16 13:09:56 -0600

    added comments

--- CSVUtility.java
+++ CSVUtility.java
@@ -12,27 +12,42 @@ public class CSVUtility {
 
 	private static HashMap<String, HashSet<CoordinateNode>> artistIndex = new HashMap<String, HashSet<CoordinateNode>>();
 
-	public static HashMap<String, HashSet<CoordinateNode>> scanner(String filePath, int minOccurance)
-			throws IOException {
+	/**
+	 * Scans input file, and reads row by row into artistIndex
+	 * 
+	 * @param filePath
+	 *            File which we are reading from
+	 * @param minOccurance
+	 *            Minimum occurrence needed for two artists
+	 * @return The map containing all artists and their corresponding row and
+	 *         column index
+	 * @throws IOException
+	 *             Error during file reading and handling
+	 */
+	public static HashMap<String, HashSet<CoordinateNode>> scanner(String filePath) throws Exception {
 
 		File fileDirectory = new File(filePath);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileDirectory), "UTF8"));
 		int curRow = 1;
-		try {
-			String curLine = reader.readLine();
-			while (curLine != null) {
-				populateMap(curLine, curRow);
-				curLine = reader.readLine();
-				curRow++;
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		} finally {
-			reader.close();
+		String curLine = reader.readLine();
+		while (curLine != null) {
+			populateMap(curLine, curRow);
+			curLine = reader.readLine();
+			curRow++;
 		}
+		reader.close();
 		return artistIndex;
 	}
 
+	/**
+	 * For each line read in scanner, line is split at comma and added to the
+	 * hashmap
+	 * 
+	 * @param curLine
+	 *            Current line
+	 * @param curRow
+	 *            Current row
+	 */
 	private static void populateMap(String curLine, int curRow) {
 		String[] artists = curLine.split(",");
 
@@ -51,6 +66,12 @@ public class CSVUtility {
 		}
 	}
 
+	/**
+	 * Writes the list to an output file
+	 * 
+	 * @param list
+	 *            The list containing the data to write in the output file
+	 */
 	public static void writer(ArrayList<ArtistNode> list) {
 		PrintStream outputFile = null;
 		try {
